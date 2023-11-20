58f49cfea3942f6649050a754986bd1cf24ee269
wayne1894 <mose286778@gmail.com>
2023-11-20 13:11:50 +0800

    no message

--- webstart/vite.config.js
+++ webstart/vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite";
+import {ViteEjsPlugin} from "vite-plugin-ejs";
 
 export default defineConfig({
+  plugins: [
+
+
+    // Or With Vite Config
+    ViteEjsPlugin((viteConfig) => {
+      // viteConfig is the current viteResolved config.
+      return {
+        root: viteConfig,
+        domain: "example.com",
+        title: "My vue project!"
+      }
+    }),
+  ],
   server: {
     open: "/index.html",
   },
